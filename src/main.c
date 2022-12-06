2021075100f82a56fe1e2f753d205587ba09deaa
Pekka Roivainen <pekka@roivainen.eu>
2022-12-06 12:32:23 +0200

    indentation fixes

--- src/main.c
+++ src/main.c
@@ -29,11 +29,11 @@ void get_pos(void);
 
 /* Set STM32 to 72 MHz. */
 static void clock_setup(void)
-{
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+    {
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
 
-	/* Enable GPIOC clock. */
-	rcc_periph_clock_enable(RCC_GPIOA);
+    /* Enable GPIOC clock. */
+    rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_GPIOB);
 
 }
@@ -49,9 +49,9 @@ static void timer_setup(void)
     timer_ic_set_input(TIM2, TIM_IC1, TIM_IC_IN_TI1);
     timer_ic_set_input(TIM2, TIM_IC2, TIM_IC_IN_TI2);
     gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
-    		      GPIO_CNF_INPUT_PULL_UPDOWN, GPIO0|GPIO1);
+                    GPIO_CNF_INPUT_PULL_UPDOWN, GPIO0|GPIO1);
     gpio_set(GPIOA, GPIO0|GPIO1);
-    
+
     timer_enable_counter(TIM2);
 
     //Setup TIM3 (PA6, PA7) to read encoder.
@@ -61,9 +61,9 @@ static void timer_setup(void)
     timer_ic_set_input(TIM3, TIM_IC1, TIM_IC_IN_TI1);
     timer_ic_set_input(TIM3, TIM_IC2, TIM_IC_IN_TI2);
     gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
-    		      GPIO_CNF_INPUT_PULL_UPDOWN, GPIO6|GPIO7);
+                    GPIO_CNF_INPUT_PULL_UPDOWN, GPIO6|GPIO7);
     gpio_set(GPIOA, GPIO6|GPIO7);
-    
+
 
     timer_enable_counter(TIM3);
 
@@ -74,8 +74,8 @@ static void timer_setup(void)
     rcc_periph_reset_pulse(RST_TIM4);
     timer_set_mode(TIM4, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
     timer_set_period(TIM4, (36*40)-1);
-	timer_disable_preload(TIM4);
-	timer_continuous_mode(TIM4);
+    timer_disable_preload(TIM4);
+    timer_continuous_mode(TIM4);
     nvic_set_priority(NVIC_TIM4_IRQ, 4);
     nvic_enable_irq(NVIC_TIM4_IRQ);
     timer_enable_irq(TIM4, TIM_DIER_UIE);
@@ -83,7 +83,7 @@ static void timer_setup(void)
 
     //initialize outputs
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_10_MHZ,
-    		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO8|GPIO9);
+                    GPIO_CNF_OUTPUT_PUSHPULL, GPIO8|GPIO9);
     gpio_clear(GPIOA, GPIO8|GPIO9);
 
 }
@@ -91,13 +91,13 @@ static void timer_setup(void)
 
 void get_pos(void)
 {
-	uint16_t now = timer_get_counter(TIM2);
-	int16_t delta =  (int16_t)(now - in1_oldcount);
+    uint16_t now = timer_get_counter(TIM2);
+    int16_t delta =  (int16_t)(now - in1_oldcount);
     in1_oldcount = now;
     in_pos += dir1*delta;
-    
+
     now = timer_get_counter(TIM3);
-	delta =  (int16_t)(now - in2_oldcount);
+    delta =  (int16_t)(now - in2_oldcount);
     in2_oldcount = now;
     in_pos += dir2*delta;
 
@@ -154,7 +154,7 @@ int main(void)
 
     while(1){
         for (int i = 0; i < 100; i++) {
-			__asm__("nop");
-		}
+            __asm__("nop");
+        }
     }
 }
